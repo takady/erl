5f9a729bc0b1c92285bf445721cb268c753068b1
Yuichi Takada <takadyuichi@gmail.com>
2014-11-20 00:10:47 +0900

    exec command

--- lib/erl/command.rb
+++ lib/erl/command.rb
@@ -2,23 +2,28 @@ require "readline"
 
 module Erl
   class Command
+    attr_reader :procs
 
     def initialize(opt)
+      @command = opt.join
       @prompt = 'erl> '
+      @procs = {}
     end
 
     def self.run(argv)
       new(argv).repl
     end
 
+    private
+
     def repl
       while buf = Readline.readline(@prompt, true)
+        Readline.completion_append_character = " "
         if %w(exit quit).include? buf
           puts "Bye"
           break
-        else
-          puts "#{buf}"
         end
+        execute buf
       end
     rescue Interrupt
       puts "\nBye"
@@ -26,5 +31,10 @@ module Erl
       abort "Error: #{e.message}"
     end
 
+    def execute(query)
+      log = "/tmp/#{Time.now.strftime('%Y%m%d%H%M%S%L')}"
+      pid = Process.spawn("echo #{query} | tr -d '\n' | #{@command}", [:out, :err]=>[log, "w"])
+      @procs[pid] = log
+    end
   end
 end
