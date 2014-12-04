952e2370209813b7015b6b0e4c56ff908b5a9457
Yuichi Takada <takadyuichi@gmail.com>
2014-12-04 23:02:45 +0900

    pty

--- lib/erl/command.rb
+++ lib/erl/command.rb
@@ -1,13 +1,11 @@
+require 'pty'
+require 'expect'
 require "readline"
 
 module Erl
   class Command
-    attr_reader :procs
-
     def initialize(opt)
       @command = opt.join
-      @prompt = 'erl> '
-      @procs = {}
     end
 
     def run
@@ -17,28 +15,24 @@ module Erl
     private
 
     def repl
-      while buf = Readline.readline(@prompt, true)
-        Readline.completion_append_character = " "
-        if %w(exit quit).include? buf
-          puts "Bye"
-          break
+      PTY.spawn(@command) do |r, w, pid|
+        w.sync = true
+        $expect_verbose = true
+
+        loop do
+          r.expect('mysql') do
+            buf = Readline.readline('> ', true)
+            w.print "#{buf}\n"
+            raise if %w(exit quit).include? buf
+          end
         end
-        execute buf
       end
+    rescue StandardError
+      puts "\nBye"
     rescue Interrupt
       puts "\nBye"
     rescue => e
       abort "Error: #{e.message}"
-    end
-
-    def execute_child(query)
-      Process.fork { @command }
-    end
-
-    def execute(query)
-      log = "/tmp/#{Time.now.strftime('%Y%m%d%H%M%S%L')}"
-      pid = Process.spawn("echo #{query} | tr -d '\n' | #{@command}", [:out, :err]=>[log, "w"])
-      @procs[pid] = log
     end
   end
 end
